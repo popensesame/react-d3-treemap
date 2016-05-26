b16108b134375a84a95f9c62c286850bb1c871a4
Matthew Geiger <mgeiger@unca.edu>
2016-05-26 13:53:01 -0400

    Fix initial d3 scales -- initial treemap layout now scales to full svg dimensions

--- src/components/treemap.jsx
+++ src/components/treemap.jsx
@@ -15,19 +15,23 @@ export default class Treemap extends React.Component {
       grandparentText: props.id(this.nodes[0]),
       parents: this.nodes.filter((d) => d.depth === 1),
       xScale: d3.scale.linear()
-        .domain([0, this.props.width])
-        .range([0, this.props.width]),
+        .domain([0, props.width])
+        .range([0, props.width]),
       yScale: d3.scale.linear()
-        .domain([0, this.props.height])
-        .range([this.props.rootHeight, this.props.height]),
+        .domain([0, props.height])
+        .range([props.rootHeight, props.height + props.rootHeight]),
     };
   }
 
   render() {
     return <svg width={this.props.width} height={this.props.height}>
       <g>
-        <g className={'grandparent'}
-           onClick={this.zoom.bind(this, this.state.grandparent, 'out')}>
+        <g
+          className={'grandparent'}
+          onClick={
+            this.state.grandparent.parent ?
+            this.zoom.bind(this, this.state.grandparent) : null
+          }>
           <rect
             className={'grandparent'}
             width={this.props.width}
@@ -44,7 +48,7 @@ export default class Treemap extends React.Component {
 
   renderNode(node) {
     if (node.children) {
-      return <g className={'children'} onClick={this.zoom.bind(this, node, 'in')}>
+      return <g className={'children'} onClick={this.zoom.bind(this, node)}>
         { this.renderRect(node, 'parent') }
         { node.children.map( (child) => this.renderRect(child, 'child') ) }
         { this.renderText(node) }
@@ -76,23 +80,24 @@ export default class Treemap extends React.Component {
     </rect>
   }
 
-  zoom(node, direction) {
-    if (direction === 'out' && node.parent) {
+  zoom(node) {
+    if (node === this.state.grandparent) {
       node = node.parent;
       this.setState({
+        depth: this.state.depth -= 1,
         grandparentText: this.state.grandparentText
           .split('.').slice(0, -1).join('.')
-      })
-    } else if (direction === 'in' && node.parent){
+      });
+    } else {
       this.setState({
+        depth: this.state.depth += 1,
         grandparentText: this.state.grandparentText
           += '.' + this.props.id(node)
-      })
+      });
     }
     this.setState({
-      depth: this.state.depth + 1,
       grandparent: node,
-      parents: node.children ? node.children : this.state.parents,
+      parents: node.children,
       xScale: d3.scale.linear()
         .domain([node.x, node.x + node.dx])
         .range([0, this.props.width])
@@ -103,8 +108,16 @@ export default class Treemap extends React.Component {
       ,
     });
   }
-
 }
+/*
+      xScale: d3.scale.linear()
+        .domain([0, this.props.width])
+        .range([0, this.props.width]),
+      yScale: d3.scale.linear()
+        .domain([0, this.props.height])
+        .range([this.props.rootHeight, this.props.height]),
+*/
+
 
 Treemap.defaultProps = {
   width: 500,
