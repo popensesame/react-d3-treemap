4b054b75069c93cbcfe912857f043f8e4175c3bc
Matthew Geiger <mgeiger>
2016-06-14 13:40:13 -0400

    remove queries.txt

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 import Treemap from './components/treemap.jsx';
 import {makeTreeFromHuc12Data, makeTreeFromHuc8Data} from './core';
 
-const huc12ChartData = require('../data/single_huc12.json').features;
 const huc8AllChartLevels = require('../data/huc8_03020104_all_chart_levels.json').features;
 
 const __tree = makeTreeFromHuc8Data(huc8AllChartLevels);
