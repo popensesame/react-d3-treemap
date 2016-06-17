f4482173d777172d7e1cf015695e2bb8365477e2
Matthew Geiger <mgeiger>
2016-06-17 15:49:41 -0400

    Build a tree for a single huc12

--- src/core.js
+++ src/core.js
@@ -1,12 +1,12 @@
 
-const DECIMAL_PRECISION = 3;
+const DECIMAL_PRECISION = 6;
 
 // Build a tree from a list of chart data for all HUC12s in a single HUC8
 // An example query's WHERE field:
 // geography_match_id='03020104' and chart_value is not null
 export const makeTreeFromHuc8Data = (data) => {
   var catUnitId,
-      tree = {
+      huc8Tree = {
         ID : 'HUC8 ' + data[0].properties.geography_match_id,
         children : [],
       },
@@ -21,18 +21,18 @@ export const makeTreeFromHuc8Data = (data) => {
     }
   });
   huc12s.forEach((arr, id) => {
-    if (arr.length === 16) tree.children.push(makeTreeFromHuc12Data(arr));
+    if (arr.length === 16) huc8Tree.children.push(makeTreeFromHuc12Data(arr));
   });
-  return tree;
+  return huc8Tree;
 }
 
 // Build a tree from a list of chart data for a single HUC12
 // Chart values are rounded and zero values are not used.
 export const makeTreeFromHuc12Data = (data) => {
   if (data[0].properties) data = data.map( (d) => d.properties);
-  var tree = data.filter( (d) => d.chart_level === 1)[0];
-  console.log(tree);
-  tree.children = [];
+  var huc12Tree = data.filter( (d) => d.chart_level === 1)[0];
+  console.log(huc12Tree);
+  huc12Tree.children = [];
   var chart_levels = new Map();
   for (var i=2; i<=4; i++) {
     chart_levels.set(i, data.filter( (d) => {
@@ -41,10 +41,10 @@ export const makeTreeFromHuc12Data = (data) => {
     }));
   }
   chart_levels.get(2).forEach((d) => {
-    tree.children.push(d);
+    huc12Tree.children.push(d);
   });
   chart_levels.get(3).forEach((d) => {
-    tree.children.forEach((e) => {
+    huc12Tree.children.forEach((e) => {
       if (d.chart_matchid === e.chart_id) {
         if (!e.children) e.children = [];
         e.children.push(d);
@@ -52,7 +52,7 @@ export const makeTreeFromHuc12Data = (data) => {
     })
   });
   chart_levels.get(4).forEach((d) => {
-    tree.children.forEach((e) => {
+    huc12Tree.children.forEach((e) => {
       if (e.children) {
         e.children.forEach((f) => {
           if (d.chart_matchid === f.chart_id) {
@@ -63,5 +63,6 @@ export const makeTreeFromHuc12Data = (data) => {
       }
     });
   });
-  return tree;
+  console.log(chart_levels);
+  return huc12Tree;
 }
--- src/index.jsx
+++ src/index.jsx
@@ -7,8 +7,8 @@ import {makeTreeFromHuc12Data, makeTreeFromHuc8Data} from './core';
 const huc12AllChartLevels = require('../data/single_huc12.json').features;
 const __huc12Tree = makeTreeFromHuc12Data(huc12AllChartLevels);
 
-const huc8AllChartLevels = require('../data/huc8_03020104_all_chart_levels.json').features
-const __huc8Tree = makeTreeFromHuc8Data(huc8AllChartLevels);
+//const huc8AllChartLevels = require('../data/huc8_03020104_all_chart_levels.json').features
+//const __huc8Tree = makeTreeFromHuc8Data(huc8AllChartLevels);
 
 const __tree = __huc12Tree;
 const props = {
